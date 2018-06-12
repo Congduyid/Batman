2e87f6992625d0bb3fdbd050f9e1601b950f63e5
Duy Bui-Cong <duybc@pc-380.local.elarion.com>
2018-06-12 15:45:30 +0700

    Them chuc nang upload hinh anh

--- Avengers/Avengers/db/schema.rb
+++ Avengers/Avengers/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_12_073410) do
+ActiveRecord::Schema.define(version: 2018_06_12_083418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 2018_06_12_073410) do
     t.string "species"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
