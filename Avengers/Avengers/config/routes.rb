4a608531c0f8b579da81c77a696f6797959b174b
Duy Bui-Cong <duybc@pc-380.local.elarion.com>
2018-06-12 15:20:08 +0700

    xuly xung dot

--- Avengers/Avengers/config/routes.rb
+++ Avengers/Avengers/config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get '/contact' => 'contact#contact'
-=======
->>>>>>> nhanlc
   resources :products
-  get 'contact/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
