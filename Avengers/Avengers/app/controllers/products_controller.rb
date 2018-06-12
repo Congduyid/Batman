ee6bfa33fd9c4d9b111315293723f4d275134d8a
lamnhan28081996 <lamnhan28081996@gmail.com>
2018-06-12 14:44:24 +0700

    Phân trang

--- Avengers/Avengers/app/controllers/products_controller.rb
+++ Avengers/Avengers/app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.all.paginate(:page => params[:page], :per_page => 5)
   end
 
   # GET /products/1
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :content, :price, :loai)
+      params.require(:product).permit(:title, :description, :image_url, :price, :species)
     end
 end
--- Avengers/Avengers/app/views/products/_product.json.jbuilder
+++ Avengers/Avengers/app/views/products/_product.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! product, :id, :name, :content, :price, :loai, :created_at, :updated_at
+json.extract! product, :id, :title, :description, :image_url, :price, :species, :created_at, :updated_at
 json.url product_url(product, format: :json)
