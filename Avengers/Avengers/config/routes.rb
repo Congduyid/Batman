8b25f17e9505eabac435d8155b3f8decef238aa7
Duy Bui-Cong <duybc@pc-380.local.elarion.com>
2018-06-13 15:13:39 +0700

    Them tim kiem trong trang products

--- Avengers/Avengers/config/routes.rb
+++ Avengers/Avengers/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/' => 'home#index'
   get '/contact' => 'contact#contact'
-  resources :products
+  resources :products do
+    collection { post :search, to: 'products#index' }
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
