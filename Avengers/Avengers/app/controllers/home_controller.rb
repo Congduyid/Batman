2db6c977f825d38b6d2d727b1bd34abc72e53b47
Duy Bui-Cong <duybc@pc-380.local.elarion.com>
2018-06-14 08:45:00 +0700

    Dem search ra ngoai home

--- Avengers/Avengers/app/controllers/home_controller.rb
+++ Avengers/Avengers/app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @search = Product.search(params[:q])
-    @products = @search.result.paginate(page: params[:page], per_page: 5)
+    if params[:query].present?
+      @products = Product.search_name(params[:query]).paginate(page: params[:page], per_page: 2)
+    else
+      @products = Product.all.paginate(page: params[:page], per_page: 2)
+    end
   end
 end
--- Avengers/Avengers/config/routes.rb
+++ Avengers/Avengers/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'home#index'
   get '/contact' => 'contact#contact'
+  get '/home' => 'home#index'
 
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
