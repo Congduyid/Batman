cec5cc6c81a739367f10b838d748c7522a56f9ef
Duy Bui-Cong <duybc@pc-380.local.elarion.com>
2018-06-14 15:17:06 +0700

    Sua loi khi qua trang 2 se ko phan loai

--- Avengers/Avengers/app/controllers/home_controller.rb
+++ Avengers/Avengers/app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
     if params[:query].present?
-      @products = Product.search_name(params[:query]).paginate(page: params[:page], per_page: 5)
+      @products = Product.where("loai_id = 1").search_name(params[:query]).paginate(page: params[:page], per_page: 5)
     else
-      @products = Product.all.paginate(page: params[:page], per_page: 2)
+      @products = Product.where("loai_id = 1").all.paginate(page: params[:page], per_page: 2)
     end
 
     if params[:query].present?
