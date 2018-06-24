5266f9074f7a25e6ba7f3625dd0b70e3bbf4c765
Congduyid <Congduyid@gmail.com>
2018-06-24 15:03:48 +0700

    cải thiện giao diện+thêm thông tin ở trang chi tiết sản phẩm.

--- Avengers/Avengers/db/schema.rb
+++ Avengers/Avengers/db/schema.rb
@@ -81,13 +81,6 @@ ActiveRecord::Schema.define(version: 2018_06_21_120117) do
     t.index ["loai_id"], name: "index_products_on_loai_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   add_foreign_key "line_items", "orders"
   add_foreign_key "products", "loais"
 end
