1022a0a67c7737aecdbc381351b67ffdec89a824
Duy Bui-Cong <duybc@pc-380.local.elarion.com>
2018-06-25 09:18:50 +0700

    Sua tim kiem tseach

--- Avengers/Avengers/app/models/product.rb
+++ Avengers/Avengers/app/models/product.rb
@@ -2,7 +2,11 @@ class Product < ApplicationRecord
     has_attached_file :image, styles: {medium: "10000x10000>", thumb: "10000x10000>"}, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
     include PgSearch
-    pg_search_scope :search_name, against: [:title, :description]
+    pg_search_scope :search_name, against: [:title, :description],   using: {
+        tsearch: {
+          prefix: true,
+        },
+      }
     belongs_to :loai
 
     validates :title, :description,:image,:price, presence: true
