3adc0dd6dfcdaf2f97341e3a56cf5ce4b354de77
Congduyid <Congduyid@gmail.com>
2018-06-27 23:59:15 +0700

    Thêm mục sản phẩm HOT bán chạy ngoài trang chủ. Sản phẩm có tick vào mục HOT sẽ đưa lên đây

--- Avengers/Avengers/db/migrate/20180612073410_create_products.rb
+++ Avengers/Avengers/db/migrate/20180612073410_create_products.rb
@@ -7,6 +7,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.decimal :price, :precision => 20, :scale => 2
       t.string :species
 
+      t.boolean :hot
       t.references :loai, foreign_key: true
       t.timestamps
     end
